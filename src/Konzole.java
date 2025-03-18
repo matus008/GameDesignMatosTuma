1e43bace7d0ec393d9227fd487eb4234b598032f
matus008 <tuma2@spsejecna.cz>
2025-03-18 16:49:54 +0100

    Dalsi cast tridy Konzole

--- src/Konzole.java
+++ src/Konzole.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Konzole {
@@ -10,6 +11,13 @@ public class Konzole {
     private Klíč klíč;
     private Křovinořez křovinořez;
     private Brian brian;
+    private Herbert herbert;
+    private Meg meg;
+    private Peter peter;
+    private Joye joye;
+    private Clevland clevland;
+    private Quagmiere quagmiere;
+
     public Konzole(MapLouder svet) {
         this.svet = svet;
         inicializace();
@@ -18,18 +26,42 @@ public class Konzole {
 
     public void executeCommand(){
     }
-    public void start(){
 
-    }
     public void inicializace(){
         Hrac hrac = new Hrac();
-        komandy.put("mluv", new BavitSeSNpc(brian));
-        komandy.put("exit", new Exit());
+        komandy.put("mluvitSBrianem", new BavitSeSNpc(brian));
+        komandy.put("mluvitSHerbertem", new BavitSeSNpc(herbert));
+        komandy.put("mluvitSMeg", new BavitSeSNpc(meg));
+        komandy.put("mluvitSPeterem", new BavitSeSNpc(peter));
+        komandy.put("mluvitSJoye", new BavitSeSNpc(joye));
+        komandy.put("mluvitSClevlandem", new BavitSeSNpc(clevland));
+        komandy.put("mluvitSQuagmirem", new BavitSeSNpc(quagmiere));
+
+
 
         komandy.put("pouzitKladivo", new PouzitPredmet(kladivo, hrac));
         komandy.put("pouzitKlic", new PouzitPredmet(klíč, hrac));
         komandy.put("pouzitKrovinorez", new PouzitPredmet(křovinořez, hrac));
 
+        komandy.put("exit", new Exit());
+    }
+    public void start(){
+        System.out.println("Vítej ve hře! Zadej příkaz:");
+        while (!exit) {
+            System.out.print("> ");
+            String input = sc.nextLine().trim();
 
+            if (komandy.containsKey(input)) {
+                String result = komandy.get(input).Execute();
+                System.out.println(result);
+            } else {
+                System.out.println("Neznámý příkaz: " + input);
+            }
+
+            if (input.equals("exit")) {
+                exit = true;
+            }
+        }
+        System.out.println("Hra ukončena.");
     }
 }
